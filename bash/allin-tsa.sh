7e98e896d39444758bdb32fa591f358f7509cf1f
Freddy Kaiser <freddy.kaiser@swisscom.com>
2013-11-27 10:33:07 +0100

    ID for TSA has no suffix

--- bash/allin-tsa.sh
+++ bash/allin-tsa.sh
@@ -12,7 +12,7 @@
 ######################################################################
 
 # AP_ID used to identify to Allin (provided by Swisscom)
-AP_ID=cartel.ch:saphir-eldiv
+AP_ID=cartel.ch
 
 ######################################################################
 # There should be no need to change anything below
